3399b179ca034ea21ce18da6df92db8bbf26f3a5
mudarAlkasem <modarAlkasem@gmail.com>
2025-10-03 17:51:06 +0300

    hotfix

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import { NEXT_AUTH_OPTIONS } from "@/lib/next-auth/auth-options";
 import { extractNextAuthRequestMetadata } from "@/lib/utils";
 import { signOutFetcher } from "@/lib/api";
+import api from "@/lib/axios";
 
 const auth = async (req: any, ctx: any) => {
   return await NextAuth(req, ctx, {
@@ -14,6 +15,19 @@ const auth = async (req: any, ctx: any) => {
       newUser: "/",
     },
     events: {
+      signIn({ user }) {
+        api.interceptors.request.use(
+          async (config) => {
+            if (user?.access_token) {
+              config.headers["Authorization"] = `Bearer ${user.access_token}`;
+            }
+
+            return config;
+          },
+          (err) => Promise.reject(err)
+        );
+        console.log(user.access_token);
+      },
       async signOut({ token }) {
         await signOutFetcher({ refresh_token: token.refresh_token as string });
       },
