2abfc1eaecc93a8ab8624f748ffca2435f8d2c20
mudarAlkasem <modarAlkasem@gmail.com>
2025-10-07 02:36:04 +0300

    feat: Implement Create from CRUD Video Operations

--- backend/apps/videos/views.py
+++ backend/apps/videos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# REST Framework Imports
+from rest_framework.request import Request
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+# Project Imports
+from core.response import Response
+
+# App Imports
+from .services.video_service import VideoService
+
+
+class VideoViewSet(ViewSet):
+    """
+    ViewSet for video CRUD operations
+    """
+
+    def create(self, request: Request) -> Response:
+        return VideoService.create(request)
