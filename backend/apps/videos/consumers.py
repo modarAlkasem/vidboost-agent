c1e0525bdaeac4c23877a41e2f34fa71574f4b61
mudarAlkasem <modarAlkasem@gmail.com>
2025-10-08 18:05:52 +0300

    feat: Send task updates constantly from Celery worker
    chore: comment in the unused code just for now

--- backend/apps/videos/consumers.py
+++ backend/apps/videos/consumers.py
@@ -48,65 +48,65 @@ class VideoWebsocketConsumer(WebsocketConsumer):
 
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    async def receive(self, text_data):
-        """Handles Websocket messages from client"""
+    # async def receive(self, text_data):
+    #     """Handles Websocket messages from client"""
 
-        try:
-            data = json.loads(text_data)
-            action = data.get("action")
+    #     try:
+    #         data = json.loads(text_data)
+    #         action = data.get("action")
 
-            if action == "get_status":
-                await self.send_task_status()
+    #         if action == "get_status":
+    #             await self.send_task_status()
 
-            else:
-                await self.send(
-                    text_data=json.dumps(
-                        {"type": "error", "message": f"Unknoow action {action}"}
-                    )
-                )
-        except json.JSONDecodeError:
-            await self.send(
-                text_data=json.dumps({"type": "error", "message": "Invalid JSON"})
-            )
+    #         else:
+    #             await self.send(
+    #                 text_data=json.dumps(
+    #                     {"type": "error", "message": f"Unknoow action {action}"}
+    #                 )
+    #             )
+    #     except json.JSONDecodeError:
+    #         await self.send(
+    #             text_data=json.dumps({"type": "error", "message": "Invalid JSON"})
+    #         )
 
-    async def send_task_status(self):
-        """Get current task status from Celery and send to client"""
-        task_status = await self.get_task_status()
-        await self.send(text_data=json.dumps(task_status))
+    # async def send_task_status(self):
+    #     """Get current task status from Celery and send to client"""
+    #     task_status = await self.get_task_status()
+    #     await self.send(text_data=json.dumps(task_status))
 
-    @database_sync_to_async
-    def get_task_status(self):
-        """Getting the status of specific Celery task"""
+    # @database_sync_to_async
+    # def get_task_status(self):
+    #     """Getting the status of specific Celery task"""
 
-        task = AsyncResult(self.task_id)
+    #     task = AsyncResult(self.task_id)
 
-        response = {
-            "type": "task_status",
-            "status": task.status,
-            "task_id": self.task_id,
-        }
+    #     response = {
+    #         "type": "task_status",
+    #         "status": task.status,
+    #         "task_id": self.task_id,
+    #     }
 
-        match task.status:
+    #     match task.status:
 
-            case "PENDING":
-                response["message"] = "Task is waiting to be executed"
+    #         case "PENDING":
+    #             response["message"] = "Task is waiting to be executed"
 
-            case "STARTED":
-                response["message"] = "Task is currently running"
+    #         case "STARTED":
+    #             response["message"] = "Task is currently running"
 
-            case "SUCCESS":
-                response["message"] = "Task completed successfully"
+    #         case "SUCCESS":
+    #             response["message"] = "Task completed successfully"
 
-            case "FAILURE":
-                response["message"] = "Task failed"
+    #         case "FAILURE":
+    #             response["message"] = "Task failed"
 
-            case "RETRY":
-                response["message"] = "Task is being retried"
+    #         case "RETRY":
+    #             response["message"] = "Task is being retried"
 
-            case _:
-                response["message"] = f"{str(task.info)}"
+    #         case _:
+    #             response["message"] = f"{str(task.info)}"
 
-        return response
+    #     return response
 
     async def task_update(self, event):
         """Receive task updates from Celery via channel layer"""
